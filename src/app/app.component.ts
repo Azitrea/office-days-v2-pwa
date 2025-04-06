6be548088328b0874123fc88c1c98a879d3fe090
azitrea <xyzddz@gmail.com>
2025-04-06 18:04:18 +0200

    firebase message subscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseService } from './service/firebase/firebase.service';
+import { Observable } from 'rxjs';
+import { MessagePayload } from 'firebase/messaging';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'office-days-v2';
 
-  constructor(private firebaseService: FirebaseService) {}
+  firebaseMessagesSignal: Signal<MessagePayload | undefined>;
+  isFirebaseMessagignActive: Signal<boolean | undefined>;
+
+  constructor(private firebaseService: FirebaseService) {
+    this.firebaseMessagesSignal = toSignal(
+      this.firebaseService.getMessagePayloadObservable()
+    );
+
+    this.isFirebaseMessagignActive = toSignal(
+      this.firebaseService.isFirebaseMessagignActive()
+    );
+  }
 
   ngOnInit(): void {
     this.firebaseService.initializeFirebase();
+    if (this.isFirebaseMessagignActive && this.isFirebaseMessagignActive()) {
+      this.subscribeToMessages();
+    }
+  }
 
-    this.firebaseService.getMessagePayloadObservable().subscribe();
-
+  subscribeToMessages(): void {
     this.firebaseService.requestPermission();
     this.firebaseService.listen();
+  }
+
+  unsubscribeFromMessages(): void {
+    this.firebaseService.deleteUserMessageSubscription();
   }
 }
--- src/app/service/firebase/firebase.service.ts
+++ src/app/service/firebase/firebase.service.ts
@@ -7,14 +7,21 @@ import {
   getToken,
   MessagePayload,
   onMessage,
+  deleteToken,
+  Unsubscribe,
 } from 'firebase/messaging';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseService {
+  private _firebaseLocalStorageKey = 'firebase-messaging-active';
+
+  private _isFirebaseMessagingActive = new BehaviorSubject<boolean>(false);
+
   private _messageSubject: Subject<MessagePayload> = new Subject();
+  private _messageUnsubscribeFunction: Unsubscribe | undefined;
 
   constructor() {}
 
@@ -22,6 +29,14 @@ export class FirebaseService {
     // Initialize Firebase
     const app = initializeApp(environment.firebase);
     const analytics = getAnalytics(app);
+
+    this._isFirebaseMessagingActive.next(
+      localStorage.getItem(this._firebaseLocalStorageKey) === 'true'
+    );
+  }
+
+  isFirebaseMessagignActive(): Observable<boolean> {
+    return this._isFirebaseMessagingActive.asObservable();
   }
 
   getMessagePayloadObservable(): Observable<MessagePayload> {
@@ -36,22 +51,41 @@ export class FirebaseService {
         if (currentToken) {
           console.log('Token');
           console.log(currentToken);
+
+          this._setFirebaseMessagingStatus(true);
         } else {
           console.log(
             'No registration token available. Request permission to generate one.'
           );
+          this._setFirebaseMessagingStatus(false);
         }
       })
       .catch((err) => {
         console.log('An error occurred while retrieving token. ', err);
+        this._setFirebaseMessagingStatus(false);
       });
   }
 
   listen() {
     const messaging = getMessaging();
-    onMessage(messaging, (payload) => {
+    this._messageUnsubscribeFunction = onMessage(messaging, (payload) => {
       console.log('Message received. ', payload);
       this._messageSubject.next(payload);
     });
+  }
+
+  async deleteUserMessageSubscription() {
+    const messaging = getMessaging();
+
+    await deleteToken(messaging);
+    if (this._messageUnsubscribeFunction) {
+      this._messageUnsubscribeFunction();
+    }
+    this._setFirebaseMessagingStatus(false);
+  }
+
+  private _setFirebaseMessagingStatus(status: boolean): void {
+    localStorage.setItem(this._firebaseLocalStorageKey, `${status}`);
+    this._isFirebaseMessagingActive.next(status);
   }
 }
